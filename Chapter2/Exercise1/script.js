7686e575f566da5b3d89bf01266eee5df5e07d74
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2021-04-25 12:04:23 +0200

    Kapitel2

---
+++ Chapter2/Exercise1/script.js
@@ -0,0 +1,12 @@
+"use strict";
+function a1() {
+    let x = "Alles";
+    console.log(x);
+    func1();
+    console.log("Logo!");
+}
+a1();
+function func1() {
+    console.log("Klar?");
+}
+//# sourceMappingURL=script.js.map
---
+++ Chapter2/Exercise1/script.ts
@@ -0,0 +1,12 @@
+function a1(): void {
+    let x: string = "Alles";
+    console.log(x);
+    func1();
+    console.log("Logo!");
+}
+
+a1();
+
+function func1(): void {
+    console.log("Klar?");
+}
