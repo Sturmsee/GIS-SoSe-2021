888467a35c8bada912a3120780652e0c9f14f584
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2021-05-02 16:05:23 +0200

    Aufgabe 2.2

--- Chapter2/Exercise2/script.js
+++ Chapter2/Exercise2/script.js
@@ -1,21 +1,20 @@
+"use strict";
 //Aufgabe 1
 //A)
-function min(array: number[]): number {
-    let small: number = array[0];
-    for (let index: number = 0; index < array.length; index++) {
+function min(array) {
+    let small = array[0];
+    for (let index = 0; index < array.length; index++) {
         if (small > array[index]) {
             small = array[index];
         }
     }
     return small;
 }
-
 console.log(min([12, 3, 3, 2, 66, 4]));
-
 //B)
-function isEven(zahl: number): boolean {
-    let even: boolean;
-    if (zahl % 2 == 1)  {
+function isEven(zahl) {
+    let even;
+    if (zahl % 2 == 1) {
         even = false;
     }
     else {
@@ -23,19 +22,16 @@ function isEven(zahl: number): boolean {
     }
     return even;
 }
-
 console.log(isEven(1));
-
 /* Bei -1 ist das Ergebnis positiv obwohl die Zahl ungerade ist. Eine Möglichkeit das zu umgehen wäre in fall von negativen Zahlen einen Zwischenschritt einzubauen,
  welcher die Zahlen vorher mit -1 multipliziert um mit positiven Zahlen zu rechnen, da dies nichts am eigentlichen Betrag der Zahlen ändern würde. */
-
 //C)
 /* interface Student {
     firstName: string;
     lastName: string;
     age: string;
     course: string;
-} 
+}
 
 var student1: Student = {firstName: "Hannes", lastName: "Regens", age: "21", course: "WING"};
 var student2: Student = {firstName: "Georg", lastName: "Schmitt", age: "26", course: "OMB"};
@@ -46,20 +42,15 @@ console.log(showInfo(student1));
 console.log(showInfo(student2));
 console.log(showInfo(student3));
 */
-
 class Student {
-    firstName: string;
-    lastName: string;
-    age: string;
-    course: string;
-    
-    constructor(firstName: string, lastName: string, age: string, course: string) {
+    constructor(firstName, lastName, age, course) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.course = course;
     }
-    showInfo(student: Student): string {
-        return student.lastName + ", " + student.firstName + ": " + student.course ;
+    showInfo(student) {
+        return student.lastName + ", " + student.firstName + ": " + student.course;
     }
 }
+//# sourceMappingURL=script.js.map
