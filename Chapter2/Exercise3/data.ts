c15be3778df4aa618493c33ca1b6c26a34fce079
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2021-05-09 16:54:34 +0200

    Update data.ts

--- Chapter2/Exercise3/data.ts
+++ Chapter2/Exercise3/data.ts
@@ -9,5 +9,3 @@ interface Choice {
 let selectHair: string = document.getElementById("hair").nodeValue;
 let selectShirt: string = document.getElementById("shirt").nodeValue;
 let selectPants: string = document.getElementById("pants").nodeValue;
-
-
