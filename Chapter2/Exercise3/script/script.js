a501c912d6ecf0f0ffeb55a1864ee03afd3e92cd
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2021-05-13 12:03:41 +0200

    Aufgabe 2.3.1

--- Chapter2/Exercise3/script/script.js
+++ Chapter2/Exercise3/script/script.js
@@ -1,19 +1,31 @@
 "use strict";
-let div = document.createElement("div");
+document.getElementById("moreBox").addEventListener("click", moreBoxes);
+document.getElementById("resetBox").addEventListener("click", resetBoxes);
 function moreBoxes() {
+    let div = document.createElement("div");
+    const boxHeight = innerHeight * Math.random();
     const boxWidth = innerWidth * Math.random();
     const boxTop = innerHeight * Math.random();
     const boxLeft = innerWidth * Math.random();
-    document.body.append(div);
-    let lastDiv = document.body.lastElementChild;
-    lastDiv.className = "divBox";
-    lastDiv.style.display = "flex";
-    lastDiv.style.width = boxWidth + "px";
-    lastDiv.style.backgroundColor = "black";
-    lastDiv.style.top = boxTop + "px";
-    lastDiv.style.left = boxLeft + "px";
+    div.classList.add("divBox");
+    div.style.height = boxHeight + "px";
+    div.style.width = boxWidth + "px";
+    div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+    div.style.top = boxTop + "px";
+    div.style.left = boxLeft + "px";
+    document.getElementById("boxInHere").appendChild(div);
 }
-function noBoxes() {
-    location.reload();
+function resetBoxes() {
+    let createdDivs = document.querySelectorAll("div.divBox");
+    for (let createdDiv of createdDivs) {
+        createdDiv.remove();
+    }
+    startUpDivs();
 }
+function startUpDivs() {
+    for (let index = 0; index < Math.floor(Math.random() * 10); index++) {
+        moreBoxes();
+    }
+}
+startUpDivs();
 //# sourceMappingURL=script.js.map
--- Chapter2/Exercise3/script/script.ts
+++ Chapter2/Exercise3/script/script.ts
@@ -1,26 +1,37 @@
+document.getElementById("moreBox").addEventListener("click", moreBoxes);
+document.getElementById("resetBox").addEventListener("click", resetBoxes);
 
-let div: HTMLDivElement = document.createElement("div");
 
 function moreBoxes(): void {
-    const boxWidth: number = innerWidth * Math.random(); 
+    let div: HTMLDivElement = document.createElement("div");
+
+    const boxHeight: number = innerHeight * Math.random();
+    const boxWidth: number = innerWidth * Math.random();
     const boxTop: number = innerHeight * Math.random();
     const boxLeft: number = innerWidth * Math.random();
-    document.body.append(div);
-    let lastDiv: HTMLDivElement = <HTMLDivElement> document.body.lastElementChild;
-    lastDiv.className = "divBox";
-    lastDiv.style.display = "flex";
-    lastDiv.style.width = boxWidth + "px";
-    lastDiv.style.backgroundColor = "black";
-    lastDiv.style.top = boxTop + "px";
-    lastDiv.style.left = boxLeft + "px";
+    
+    div.classList.add("divBox");
+    div.style.height = boxHeight + "px";
+    div.style.width = boxWidth + "px";
+    div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+    div.style.top = boxTop + "px";
+    div.style.left = boxLeft + "px";
+    
+    document.getElementById("boxInHere").appendChild(div);
 }
 
-function noBoxes(): void {
-    location.reload();
+function resetBoxes(): void {
+    let createdDivs: NodeListOf<Element> = document.querySelectorAll("div.divBox"); 
+    for (let createdDiv of createdDivs) {
+        createdDiv.remove();
+    }
+    startUpDivs();
 }
 
-interface Choice {
-    hair: string;
-    shirt: string;
-    pants: string;
+function startUpDivs(): void {
+    for (let index: number = 0; index < Math.floor(Math.random() * 10); index++) {
+        moreBoxes();
+    }
 }
+
+startUpDivs();
