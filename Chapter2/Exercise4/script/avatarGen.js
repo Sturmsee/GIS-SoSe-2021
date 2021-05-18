0d1711f786d360ea8c06a4100ffed73022ff7ebb
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2021-05-18 18:44:31 +0200

    fix

--- Chapter2/Exercise4/script/avatarGen.js
+++ Chapter2/Exercise4/script/avatarGen.js
@@ -1,34 +1,35 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data_1 = require("./data");
-function createChoices(_parts) {
-    let div = document.createElement("div");
-    div.classList.add("choices");
-    let img = document.createElement("img");
-    img.src = _parts.image;
-    div.appendChild(img);
-    let choice = document.createElement("span");
-    choice.innerText = _parts.part;
-    div.appendChild(choice);
-    let button = document.createElement("button");
-    button.innerText = "Select";
-    button.addEventListener("click", subEvent);
-    return div;
-    function subEvent(_e) {
-        console.log("innere Funktion", _parts);
-        intoJSON(_parts);
+var Exercise2_4;
+(function (Exercise2_4) {
+    function createChoices(_parts) {
+        let div = document.createElement("div");
+        div.classList.add("choices");
+        let img = document.createElement("img");
+        img.src = _parts.image;
+        div.appendChild(img);
+        let choice = document.createElement("span");
+        choice.innerText = _parts.part;
+        div.appendChild(choice);
+        let button = document.createElement("button");
+        button.innerText = "Select";
+        button.addEventListener("click", subEvent);
+        return div;
+        function subEvent(_e) {
+            console.log("innere Funktion", _parts);
+            intoJSON(_parts);
+        }
     }
-}
-function intoJSON(_input) {
-    let input = JSON.stringify(_input);
-    sessionStorage.setItem("selected", input);
-}
-function showSelectableParts(_parts) {
-    let allChoices = document.getElementById("choicesDiv");
-    for (let _part of _parts) {
-        let div = createChoices(_part);
-        allChoices.appendChild(div);
+    function intoJSON(_input) {
+        let input = JSON.stringify(_input);
+        sessionStorage.setItem("selected", input);
     }
-}
-showSelectableParts(data_1.allParts.faces);
+    function showSelectableParts(_parts) {
+        let allChoices = document.getElementById("choicesDiv");
+        for (let _part of _parts) {
+            let div = createChoices(_part);
+            allChoices.appendChild(div);
+        }
+    }
+    showSelectableParts(Exercise2_4.allParts.faces);
+})(Exercise2_4 || (Exercise2_4 = {}));
 //# sourceMappingURL=avatarGen.js.map
--- Chapter2/Exercise4/script/data.js
+++ Chapter2/Exercise4/script/data.js
@@ -1,24 +1,25 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allParts = void 0;
-exports.allParts = {
-    faces: [
-        { image: "#", part: "RedNoise/BraidedHair" },
-        { image: "#", part: "ShortNose/SpikedHair" },
-        { image: "#", part: "StubbyNose/ShortHair" },
-        { image: "#", part: "NoNose/NoHair" }
-    ],
-    shirts: [
-        { image: "#", part: "LongSleeve/Red" },
-        { image: "#", part: "LongSleeve/Green" },
-        { image: "#", part: "ShortSleeve/Black" },
-        { image: "#", part: "ShortSleeve/Blue" }
-    ],
-    pants: [
-        { image: "#", part: "Jeans/Blue" },
-        { image: "#", part: "Jeans/Black" },
-        { image: "#", part: "Shorts/Green" },
-        { image: "#", part: "Shorts/Brown" }
-    ]
-};
+var Exercise2_4;
+(function (Exercise2_4) {
+    Exercise2_4.allParts = {
+        faces: [
+            { image: "#", part: "RedNoise/BraidedHair" },
+            { image: "#", part: "ShortNose/SpikedHair" },
+            { image: "#", part: "StubbyNose/ShortHair" },
+            { image: "#", part: "NoNose/NoHair" }
+        ],
+        shirts: [
+            { image: "#", part: "LongSleeve/Red" },
+            { image: "#", part: "LongSleeve/Green" },
+            { image: "#", part: "ShortSleeve/Black" },
+            { image: "#", part: "ShortSleeve/Blue" }
+        ],
+        pants: [
+            { image: "#", part: "Jeans/Blue" },
+            { image: "#", part: "Jeans/Black" },
+            { image: "#", part: "Shorts/Green" },
+            { image: "#", part: "Shorts/Brown" }
+        ]
+    };
+})(Exercise2_4 || (Exercise2_4 = {}));
 //# sourceMappingURL=data.js.map
