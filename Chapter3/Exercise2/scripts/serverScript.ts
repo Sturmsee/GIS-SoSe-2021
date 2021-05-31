0bc622c4b9088012021662e07e52d575ebeaa714
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2021-05-31 15:26:04 +0200

    start

---
+++ Chapter3/Exercise2/scripts/serverScript.ts
@@ -0,0 +1,42 @@
+import * as Http from "http";
+import * as url from "url";
+
+export namespace Exercise3_2 {
+    console.log("Starting server");
+    let port: number = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+
+    let server: Http.Server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    let adresse: string = 'http://localhost:8080/default.htm?jahr=2017&monat=february';
+    //Adresse parsen (umwandeln):
+    let q = url.parse(adresse, true);
+
+    /*Die parse Methode gibt ein Objekt zurück, dass die URL Eigenschaften enthält. So können die fest definierten Eigenschaften einer URL ausgelesen werden:*/
+    console.log(q.host);
+    console.log(q.pathname);
+    console.log(q.search);
+
+    /*Die query Eigenschaft gibt ein Ojekt zurück, dass alle query-string Parameter als Eigenschaften besitzt. So können beliebig gesendete Attribute ausgelesen werden:*/
+    var qdata = q.query;
+    console.log(qdata.monat);
+
+    function handleListen(): void {
+        console.log("Listening");
+    }
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!");
+
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+
+        _response.write(_request.url);
+
+        _response.end();
+    }
+}
